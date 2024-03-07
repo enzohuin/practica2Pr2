5f467e8bc833d3c0bc1ff99a145531e6dc993e59
enzohuin <133805188+enzohuin@users.noreply.github.com>
2024-03-07 13:35:00 +0100

    Update javadoc

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,9 +1,17 @@
 package aplicacion;
 
 import mates.Matematicas;
-
+/**
+ * Clase principal para calcular una aproximación al número Pi utilizando el método de Montecarlo de forma recursiva.
+ * Se espera un argumento de la línea de comandos que indica la cantidad de puntos a generar para la aproximación de Pi.
+ * Para ejecutar la aplicación: {@code java aplicacion.Principal <numero_pasos>}.
+ */
 public class Principal {
     public static void main(String[] args) {
+    /**
+     * Punto de entrada principal para ejecutar el programa.
+     * Se espera un argumento que indica la cantidad de pasos.
+     */
         System.out.println("El número PI es " + Matematicas.generarNumeroPiRecursivo(Integer.parseInt(args[0])));
     }
 }
