6490745be5c97ea2885a9a0fcc2b593232172a7f
enzohuin <133805188+enzohuin@users.noreply.github.com>
2024-03-14 11:01:20 +0100

    Update package-info.java

--- src/aplicacion/package-info.java
+++ src/aplicacion/package-info.java
@@ -2,4 +2,4 @@
  *Este paquete contiene la clase principal del programa y se encarga de mostrar los resultados de las aproximaciones de Pi.
  *Es la interfaz de usuario para utilizar las funciones de la clase Matematicas
  */
-  package aplicacion
+  package aplicacion;
